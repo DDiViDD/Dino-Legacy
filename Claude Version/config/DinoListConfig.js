217846c73065bf3f0530767010a19df163d8b557
Realpra1 <realpra1@hotmail.com>
2026-05-06 12:55:36 +0200

    Delete Claude Version/config/DinoListConfig.js

--- Claude Version/config/DinoListConfig.js
+++
@@ -1,7 +0,0 @@
-// DinoListConfig: list of active species. Each name must match a file
-// in model/dinos/ and a script tag in index.html.
-
-var DinoListConfig = [
-    'Brachiosaur',
-    // 'TRex',
-];
